08e7d5135e97417d3c303333e9522d33eb814a73
StompyNZ <russell@codestorm.nz>
2016-09-15 18:18:04 +1200

    Added AuthNav ViewComponent

---
+++ src/7days2mod-core/ViewComponents/AuthNavViewComponent.cs
@@ -0,0 +1,53 @@
+﻿using _7days2mod_core.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace _7days2mod_core.ViewComponents
+{
+    public class AuthNavData
+    {
+        public string token;
+        public string accessURL;
+        public string logoutURL;
+        public string profileURL;
+        public string avatarURL;
+        public string username;
+    }
+
+    public class AuthNavViewComponent : ViewComponent
+    {
+        private readonly AppSettings _appSettings;
+
+        public AuthNavViewComponent(
+            IOptions<AppSettings> appSettings
+            )
+        {
+            _appSettings = appSettings.Value;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            AuthNavData _authNavData = new AuthNavData();
+            try
+            {
+                _authNavData.token = HttpContext.Session.GetString("access_token");
+            }
+            catch
+            {
+                return View("NoAuth");
+            }
+            if (String.IsNullOrEmpty(_authNavData.token))
+            {
+                return View("NoAuth");
+            } else
+            {
+                _authNavData.accessURL = "https://github.com/settings/connections/applications/" + _appSettings.GitHubClientId;
+                _authNavData.logoutURL = "/logout";
+
+                return View("Authorized", _authNavData);
+            }
+        }
+    }
+}
